aec6bfd5d95b76245b6920773b241e0be463afd4
Uwe Riegel <uriegel@hotmail.de>
2021-02-14 16:58:35 +0100

    Converting to typescript

--- example/src/index.js
+++ example/src/index.js
@@ -1,7 +1,6 @@
-import './index.css'
-
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './App'
-
-ReactDOM.render(<App />, document.getElementById('root'))
+import './index.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+ReactDOM.render(React.createElement(React.StrictMode, null,
+    React.createElement(App, null)), document.getElementById('root'));
