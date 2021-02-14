7059c9ca5dc87670e1c3f4f36011f971325a2539
Uwe Riegel <uriegel@hotmail.de>
2021-02-14 18:03:51 +0100

    obsolete

--- example/src/index.js
+++
@@ -1,6 +0,0 @@
-import './index.css';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-ReactDOM.render(React.createElement(React.StrictMode, null,
-    React.createElement(App, null)), document.getElementById('root'));
