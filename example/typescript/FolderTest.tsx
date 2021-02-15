f20fff550d3520b73bd51c5ad4378557043e2634
Uwe Riegel <uriegel@hotmail.de>
2021-02-15 12:59:31 +0100

    FolderTable with items and Columns

--- example/typescript/FolderTest.tsx
+++ example/typescript/FolderTest.tsx
@@ -1,11 +1,53 @@
-import React from 'react'
-import { FolderTable } from 'file-commander-control'
+import React, { useState } from 'react'
+import { FolderTable  } from 'file-commander-control'
+import { VirtualTableItem, VirtualTableItems, Column } from 'virtual-table-react'
 
+
+interface FolderItem extends VirtualTableItem {
+    col1: string
+    col2: string
+    col3: string
+}
 type FolderTestProps = {
     theme: string
 }
 export const FolderTest = ({theme}: FolderTestProps) => {
+    const [focused, setFocused] = useState(false)
+
+    const onSetFocus = () => {
+        setFocused(true)
+    }   
+
+    const [columns, setColumns] = useState([
+        { name: "Eine Spalte", isSortable: true }, 
+        { name: "Zweite. Spalte" }, 
+        { name: "Letzte Spalte", isSortable: true }
+    ] as Column[])
+
+    const onColsChanged = (cols: Column[])=> {}
+    const onSort = ()=> {}
+
+    const [items, setItems ] = useState({count: 0, getItem: (i: number)=>{}} as VirtualTableItems)
+    const onChange = () => setItems({count: 30, getItem, itemRenderer})
+
+    const getItem = (index: number) => ({ col1: `Name ${index}`, col2: `Adresse ${index}`, col3: `Größe ${index}`, index: index} as FolderItem)
+
+    const itemRenderer = (item: VirtualTableItem) => {
+        const tableItem = item as FolderItem
+        return [
+            <td key={1}>{tableItem.col1}</td>,
+            <td key={2}>{tableItem.col2}</td>,
+            <td key={3}>{tableItem.col3}</td>	
+	    ]
+    }
+
     return (	
-        <FolderTable theme={theme} /> 
+        <div className={"folderTest"}>
+            <button onClick={onChange}>Fill</button>
+            <button onClick={onSetFocus}>Set Focus</button>
+            <FolderTable theme={theme} focused={focused} setFocused={setFocused} 
+                columns={columns} onColumnsChanged={onColsChanged} onSort={onSort}
+                items={items} /> 
+        </div>
 	)
 }
--- typescript/FolderTable.tsx
+++ typescript/FolderTable.tsx
@@ -1,65 +1,31 @@
 import React, { useState } from 'react'
 import 'virtual-table-react/dist/index.css'
+import { Column, VirtualTable, VirtualTableItem, VirtualTableItems } from 'virtual-table-react'
+
 // @ts-ignore
 import styles from './styles.module.css'
 
-
-import { Column, VirtualTable, VirtualTableItems, VirtualTableItem } from 'virtual-table-react'
-
-interface TableItem extends VirtualTableItem {
-    col1: string
-    col2: string
-    col3: string
-}
-
 type FolderTableProps = {
     theme?: string
+    focused: boolean
+    setFocused: (val: boolean)=>void
+    columns: Column[]
+    onColumnsChanged: (cols: Column[])=> {}
+    onSort: ()=> {}
+    items: VirtualTableItems
 }
 
-export const FolderTable = ({theme}: FolderTableProps) => {
-    const [cols, setCols] = useState([
-        { name: "Eine Spalte", isSortable: true }, 
-        { name: "Zweite. Spalte" }, 
-        { name: "Letzte Spalte", isSortable: true }
-    ] as Column[])
-    const [focused, setFocused] = useState(false)
-    const [items, setItems ] = useState({count: 0, getItem: (i: number)=>{}} as VirtualTableItems)
-    const onColsChanged = (cols: Column[])=> {}
-    const onSort = ()=> {}
-
-    const getItem = (index: number) => ({ col1: `Name ${index}`, col2: `Adresse ${index}`, col3: `Größe ${index}`, index: index} as TableItem)
-    const onChange = () => setItems({count: 30, getItem})
-    
-    const itemRenderer = (item: VirtualTableItem) => {
-        const tableItem = item as TableItem
-        return [
-            <td key={1}>{tableItem.col1}</td>,
-            <td key={2}>{tableItem.col2}</td>,
-            <td key={3}>{tableItem.col3}</td>	
-	    ]
-    }
-
-    const onSetFocus = () => {
-        setFocused(true)
-    }   
-
-    const onFocused = (val: boolean) => setFocused(val)
-
+export const FolderTable = ({theme, focused, setFocused, columns, onColumnsChanged, onSort, items}: FolderTableProps) => {
     return (
-        <div className={styles.rootVirtualTable}>
-            <h1>Virtual Table</h1>
-            <button onClick={onChange}>Fill</button>
-            <button onClick={onSetFocus}>Set Focus</button>
-            <div className={styles.containerVirtualTable}>
-                <VirtualTable 
-                    columns={cols} 
-                    onColumnsChanged={onColsChanged} 
-                    onSort={onSort} items={items} 
-                    itemRenderer={itemRenderer}
-                    theme={theme}
-                    focused={focused}
-                    onFocused={onFocused} />
-            </div>
+        <div className={styles.containerVirtualTable}>
+            <VirtualTable 
+                columns={columns} 
+                onColumnsChanged={onColumnsChanged} 
+                onSort={onSort} items={items} 
+                itemRenderer={items.itemRenderer}
+                theme={theme}
+                focused={focused}
+                onFocused={setFocused} />
         </div>
     )
 }
