6cde7f43e52a3f7f40ee7bef7f8444d60b4504d7
Uwe Riegel <uriegel@hotmail.de>
2021-02-18 15:50:23 +0100

    Set selections

--- typescript/FolderTable.tsx
+++ typescript/FolderTable.tsx
@@ -36,9 +36,37 @@ export const FolderTable = ({
 
     const onKeyDown = (sevt: React.KeyboardEvent) => {
         const evt = sevt.nativeEvent
-        if (evt.which == 45) {
+        if (evt.which == 35 && evt.shiftKey) { // Shift + end
+            items.items = items.items.map((n, i) => { 
+                n.isSelected = i >= (items.currentIndex ?? 0) 
+                return n
+            }) 
+            onItemsChanged(setVirtualTableItems(items))
+        }
+        if (evt.which == 36 && evt.shiftKey) { // Shift + home
+            items.items = items.items.map((n, i) => { 
+                n.isSelected = i <= (items.currentIndex ?? 0) 
+                return n
+            }) 
+            onItemsChanged(setVirtualTableItems(items))
+        }
+        if (evt.which == 45) { // Ins
             items.items[items.currentIndex ?? 0].isSelected = !items.items[items.currentIndex ?? 0].isSelected
             items.currentIndex = (items.currentIndex ?? 0) + 1
+            onItemsChanged(setVirtualTableItems(items))
+        }
+        if (evt.which == 107) { // Numlock +
+            items.items = items.items.map(n => { 
+                n.isSelected = true 
+                return n
+            }) 
+            onItemsChanged(setVirtualTableItems(items))
+        }
+        if (evt.which == 109) { // Numlock -
+            items.items = items.items.map(n => { 
+                n.isSelected = false 
+                return n
+            }) 
             onItemsChanged(setVirtualTableItems(items))
         }
     }
@@ -59,4 +87,14 @@ export const FolderTable = ({
     )
 }
 
+// TODO textbox containing path, enter
+// TODO Restriction
+// TODO Grid splitter type script
+// TODO two folder items
+// TODO F3 viewer
 
+// TODO in hyper rename fill directory items and drive items and icons
+
+
+// TODO parent item not selectable: isSelectable property per folder item
+// TODO Exif info in anoter style
