2ab2523806910df44743d4c9e1c06a5698d92197
Uwe Riegel <uriegel@hotmail.de>
2021-03-13 07:12:36 +0100

    Separate states for left and right folder

--- example/typescript/Commander.tsx
+++ example/typescript/Commander.tsx
@@ -1,11 +1,24 @@
 import React, { useState, useRef } from 'react'
 import { SplitterGrid } from 'grid-splitter-react'
+import { FolderTableItem } from '../../dist/FolderTable'
+
+interface FolderItem extends FolderTableItem {
+    index: number
+    col2: string
+    col3: string
+}
 
 type CommanderProps = {
     theme: string
 }
 
 export const Commander = ({theme}: CommanderProps) => {
+    const [focusedLeft, setFocusedLeft] = useState(false)
+    const [pathLeft, setPathLeft] = useState(["", ""])
+
+    const [focusedRight, setFocusedRight] = useState(false)
+    const [pathRight, setPathRight] = useState(["", ""])
+
     return (	
         <SplitterGrid first={<div>Hallo</div>} second={<div>Welt</div>} />
     )
